e89a3aba51f346e382ba274c7232c1fa5f556974
Nastusha <anastasiya.kalugina.2013@mail.ru>
2024-04-22 16:08:06 +0300

    Fix the problems with spsces, added a func that checks for an empty str, delegated part of func

--- calculator/calculator.cpp
+++ calculator/calculator.cpp
@@ -1,116 +1,142 @@
 ﻿#include <iostream>
 #include <stack>
-#include<string>
+#include <string>
 bool isOperator(char c)
 {
-    return c == '+' || c == '-' || c == '*' || c == '/';
+	return c == '+' || c == '-' || c == '*' || c == '/';
 }
 
 bool isOperand(char c)
 {
-    return isdigit(c);
+	return isdigit(c);
 }
 
 int precedence(char c)
 {
-    if (c == '+' || c == '-') return 1;
-    if (c == '*' || c == '/') return 2;
+	if (c == '+' || c == '-') return 1;
+	if (c == '*' || c == '/') return 2;
+}
+
+void Count(std::stack<double>& operands, std::string token, char token_0)
+{
+	double secondOperand = operands.top();
+	operands.pop();
+	double firstOperand = operands.top();
+	operands.pop();
+	//char op = token[0];
+
+	double result;
+	switch (token_0)
+	{
+	case '+':
+		result = firstOperand + secondOperand;
+		break;
+	case '-':
+		result = firstOperand - secondOperand;
+		break;
+	case '*':
+		result = firstOperand * secondOperand;
+		break;
+	case '/':
+		result = firstOperand / secondOperand;
+		break;
+	}
+	operands.push(result);
 }
 
 std::string infixToRPN(const std::string& expression)
 {
-    std::stack<char> operators;
-    std::string output;
-    for (size_t i = 0; i < expression.size(); i++)
-    {
-        if (expression[i] == ' ')
-            continue;
+	std::stack<char> operators;
+	std::string output;
+	for (size_t i = 0; i < expression.size(); i++)
+	{
+		if (expression.std::string::empty())
+			return "";
 
-        if (isOperand(expression[i]))
-        {
-            std::string operand;
-            while (i < expression.size() && isOperand(expression[i]))
-            {
-                operand += expression[i++];
-            }
-            output += operand + ' '; //пробелы между операндами и операциями
-            //--i; //WHY???
-        }
-        else if (isOperator(expression[i]))
-        {
-            while (!operators.empty() && precedence(expression[i]) <= precedence(operators.top()))
-            {
-                output += operators.top();
-                operators.pop();
-                output += ' ';
-            }
-            operators.push(expression[i]); // если оператор по приоритету больше то просто добавляем без проверки
-        }
-    }
-    while (!operators.empty())
-    {
-        output += operators.top();
-        operators.pop();
-        output += ' ';
-    }
-    return output;
+		if (expression[i] == ' ')
+			continue;
+
+		if (isOperand(expression[i]))
+		{
+			std::string operand;
+			while (i < expression.size() && isOperand(expression[i]))
+				operand += expression[i++];
+			--i;
+			output += operand + ' '; //пробелы между операндами и операциями
+		}
+
+		else if (isOperator(expression[i]))
+		{
+			while (!operators.empty() && precedence(expression[i]) <= precedence(operators.top()))
+			{
+				output += operators.top();
+				operators.pop();
+				output += ' ';
+			}
+			operators.push(expression[i]); // если оператор по приоритету больше то просто добавляем без проверки
+		}
+	}
+
+	while (!operators.empty())
+	{
+		output += operators.top();
+		operators.pop();
+		output += ' ';
+	}
+
+	return output;
 }
 
 double evaluateRPN(const std::string& rpn)
 {
-    std::stack<double> operands;
-    size_t pos = 0;
-    while (pos < rpn.size())
-    {
-        if (rpn[pos] == ' ')
-        { ++pos; continue; }
+	std::stack<double> operands;
+	size_t pos = 0;
 
-        std::string token;
-        while (pos < rpn.size() && rpn[pos] != ' ')
-            token += rpn[pos++];
+	while (pos < rpn.size())
+	{
+		if (rpn[pos] == ' ')
+		{
+			++pos;
+			continue;
+		}
 
-        if (isOperand(token[0]))
-            operands.push(std::stod(token));
+		std::string token;
+		while (pos < rpn.size() && rpn[pos] != ' ')
+			token += rpn[pos++];
+		char token_0 = token[0];
 
-        else if (isOperator(token[0]))
-        {
-            double secondOperand = operands.top();
-            operands.pop();
-            double firstOperand = operands.top();
-            operands.pop();
-            char op = token[0];
+		if (isOperand(token_0))
+			operands.push(std::stod(token));
 
-            double result;
-            switch (op)
-            {
-            case '+':
-                result = firstOperand + secondOperand;
-                break;
-            case '-':
-                result = firstOperand - secondOperand;
-                break;
-            case '*':
-                result = firstOperand * secondOperand;
-                break;
-            case '/':
-                result = firstOperand / secondOperand;
-                break;
-            }
-            operands.push(result);
-        }
-        ++pos;
-    }
-    return operands.top();
+		else if (isOperator(token_0))
+		{
+			Count(operands, token, token_0);
+		}
+		++pos;
+	}
+
+	return operands.top();
+}
+
+
+int Сalculate()
+{
+	setlocale(LC_ALL, "rus");
+	std::cout << "Введите математическое выражение: ";
+	std::string example;
+	getline(std::cin, example);
+	std::string RPN = infixToRPN(example);
+	if (RPN == "")
+	{
+		std::cout << "Пустая строка";
+		return 0;
+	}
+	double answer = evaluateRPN(RPN);
+	std::cout << "Ответ: " << answer;
+	return answer;
 }
 
 int main()
-{ 
-    setlocale(LC_ALL, "rus");
-    std::string example;
-    std::cout << "Введите математический пример (между операндами и операторами пробел): ";
-    getline(std::cin, example);
-    std::string RPN = infixToRPN(example);
-    double answer = evaluateRPN(RPN);
-    std::cout << "Ответ: " << answer;
-    
+{
+	Сalculate();
 }
